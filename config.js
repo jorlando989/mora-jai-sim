a6d2164289f2ec1e18fd79ff329162101f50f1bd
jorlando989 <jorlando989@gmail.com>
2026-09-01 16:24:56 -0400

    debugging envs

--- config.js
+++ config.js
@@ -7,5 +7,6 @@ export const API_ENDPOINT = isLocalHost
 	? "http://127.0.0.1:8000"
 	: "https://morajaidle-b93592b4c247.herokuapp.com";
 
+console.log("Host:", window.location.hostname);
 console.log("Environment:", isLocalHost ? "Local" : "Production");
 console.log("API Endpoint:", API_ENDPOINT);
